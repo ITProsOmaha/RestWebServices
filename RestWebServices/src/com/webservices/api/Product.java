faf951c4dfb384e011b2f28e33cbfad31ed98776
raavi <raavi.gayathri@gmail.com>
2016-12-21 21:29:07 -0600

    Added productName

--- RestWebServices/src/com/webservices/api/Product.java
+++ RestWebServices/src/com/webservices/api/Product.java
@@ -6,16 +6,16 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-@Path("/product/{ProductId}")
+@Path("/product")
 public class Product {
 	
-	/*@GET
+	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String productName(){
 		return "My name is ipad 2";
-	}*/
+	}
 	
-	@GET
+	/*@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String productNameByID(@PathParam("ProductId") String id){
 		if (id.equals("1"))
@@ -23,6 +23,28 @@ public class Product {
 		else
 			return "No Product";
 		
-	}
+	}*/
+	
+	@GET
+	@Path("{productId}")
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.TEXT_PLAIN)
+	public String productId(@PathParam("productId") int id){
+		if(id == 1)
+			return "id is "+"1";
+		else 
+			return "no productId";
+	}	
+	@GET
+	@Path("/productname/{productName}")
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.TEXT_PLAIN)
+	public String productName(@PathParam("productName") String name){
+		
+			return "Hello "+name;
+				
+}
+	
+	
 	
 }
